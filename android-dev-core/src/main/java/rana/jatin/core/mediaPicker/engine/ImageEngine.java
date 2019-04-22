d800c72a78ed55cb51a241e533382a19c1db4fbb
jatin <thisisjatinrana@gmail.com>
2019-04-22 13:51:48 +0530

    Project migrated to androidx

--- android-dev-core/src/main/java/rana/jatin/core/mediaPicker/engine/ImageEngine.java
+++ android-dev-core/src/main/java/rana/jatin/core/mediaPicker/engine/ImageEngine.java
@@ -20,10 +20,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.widget.ImageView;
 
-/**
- * Image loader interface. There are predefined {@link com.zhihu.matisse.engine.impl.GlideEngine}
- * and {@link com.zhihu.matisse.engine.impl.PicassoEngine}.
- */
+
 @SuppressWarnings("unused")
 public interface ImageEngine {
 
